4616f61e78e589189f326fa3f16091a0ab2f6ad4
lmsand <lmsand99@icloud.com>
2023-12-13 22:20:47 -0600

    Initial Commit

---
+++ TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by lexi sanders on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
