968014213d30a437a62cb5ce91d1fc036d5d44d7
lmsand <lmsand99@icloud.com>
2023-12-14 00:26:45 -0600

    ExploreView

---
+++ TikTokTutorial/Core/Search/ExploreView.swift
@@ -0,0 +1,30 @@
+//
+//  SearchView.swift
+//  TikTokTutorial
+//
+//  Created by lexi sanders on 12/14/23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(0..<20) { user in
+                        UserCell()
+                            .padding(.horizontal)
+                    }
+                }
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.top)
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
